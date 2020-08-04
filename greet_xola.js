95c561a53d4a4ba124672ef9a64f0907a6c7c210
Cebi-coder <cebisapeter40@gmail.com>
2020-08-04 14:17:59 +0200

    styled using figlet

--- greet_xola.js
+++ greet_xola.js
@@ -2,16 +2,18 @@ const chalk = require('chalk');
 // import the greet module that is in the current folder
 const greet = require('./greet');
 
-const styledMessage = chalk.bgGreen.black(greet('Xola'));
- console.log(styledMessage)
+
+ 
 
  var figlet = require('figlet');
  
-figlet('Hello World!!', function(err, data) {
+figlet(greet('Xola'), function(err, data) {
     if (err) {
         console.log('Something went wrong...');
         console.dir(err);
         return;
     }
-    console.log(data)
+    
+    const styledMessage = chalk.bgGreen.black(data);
+    console.log(styledMessage)
 });
